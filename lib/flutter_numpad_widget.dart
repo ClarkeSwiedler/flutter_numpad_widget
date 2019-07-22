d92e01915848b0d8c630e44f677d222b47f86709
Clarke Swiedler <Clarke@swiedler.com>
2019-07-22 16:47:11 -0400

    Prepare for publishing.

--- lib/flutter_numpad_widget.dart
+++ lib/flutter_numpad_widget.dart
@@ -1,11 +1,9 @@
 ///This library aims to provide a straightforward, configurable number pad 
 ///widget.
-///
-///The [Numpad] can be used alone
 
 library flutter_numpad_widget;
 
 export './src/numpad.dart';
 export './src/numpad_controller.dart';
 export './src/numpad_text.dart';
-export 'package:flutter_numpad_widget/src/formatting/numpad_format.dart';
+export './src/formatting/numpad_format.dart';
